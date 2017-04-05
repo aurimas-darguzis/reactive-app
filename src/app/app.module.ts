5a69707d658b0a08f786fb5ba0879719ac9f958d
aurimas-darguzis <aurimas@darguzis.com>
2017-04-05 13:03:12 +0100

    added models to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,29 @@ import { Ng2RestAppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ItemDetailComponent } from './items/item-detail/item-detail.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetDetailComponent } from './widgets/widget-detail/widget-detail.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ItemsComponent,
+    ItemsListComponent,
+    ItemDetailComponent,
+    UsersComponent,
+    UserDetailComponent,
+    UsersListComponent,
+    WidgetsComponent,
+    WidgetDetailComponent,
+    WidgetsListComponent
   ],
   imports: [
     BrowserModule,
