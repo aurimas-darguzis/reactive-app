9a59a26203f4147d8bc472e7537e8d623e27bf6f
aurimas-darguzis <aurimas@darguzis.com>
2017-04-06 13:38:36 +0100

    implemented user detail component class

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../../shared';
+import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  styleUrls: ['./user-detail.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserDetailComponent implements OnInit {
+export class UserDetailComponent {
+  originalName: string;
+  selectedUser: User;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  @Input() set user(value: User) {
+    if (value) { this.originalName = value.name }
+    this.selectedUser = Object.assign({}, value);
   }
-
 }
