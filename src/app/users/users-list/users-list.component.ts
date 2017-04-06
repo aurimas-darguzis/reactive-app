eb6efd147bc96d7fc02af03a59ed4f038c42552e
aurimas-darguzis <aurimas@darguzis.com>
2017-04-06 13:34:52 +0100

    implemented user-list component and template

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { User } from '../../shared';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UsersListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class UsersListComponent {
+  @Input() users: User[];
+  @Output() selected = new EventEmitter();
+  @Output() deleted = new EventEmitter();
 }
