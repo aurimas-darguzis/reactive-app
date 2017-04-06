452157bcc1aef4b6a3087fc1c5960a11f378d828
aurimas-darguzis <aurimas@darguzis.com>
2017-04-06 10:36:15 +0100

    created widgets reducer and service

--- src/app/app-store.ts
+++ src/app/app-store.ts
@@ -1,7 +1,9 @@
 import { User } from './shared/user.model';
 import { Item } from './shared/item.model';
+import { Widget } from './shared/widget.model';
 
 export interface AppStore {
     items: Item[];
     user: User[];
+    widget: Widget[];
 }
