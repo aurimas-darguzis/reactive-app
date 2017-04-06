5245f7f0f4aaef7ff933fb231b75fc45e3612328
aurimas-darguzis <aurimas@darguzis.com>
2017-04-06 11:59:44 +0100

    implemented widget-detail component

--- src/app/widgets/widget-detail/widget-detail.component.ts
+++ src/app/widgets/widget-detail/widget-detail.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Widget, UsersService, User } from '../../shared';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-widget-detail',
   templateUrl: './widget-detail.component.html',
-  styleUrls: ['./widget-detail.component.css']
+  styleUrls: ['./widget-detail.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WidgetDetailComponent implements OnInit {
+  originalName: string;
+  selectedWidget: Widget;
+  users$: Observable<User[]> = this.usersSerivce.users$;
 
-  constructor() { }
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
+
+  @Input() set widget(value: Widget) {
+    if (value) { this.originalName = value.name; }
+    this.selectedWidget = Object.assign({}, value);
+  }
+
+  constructor(private usersSerivce: UsersService) { }
 
   ngOnInit() {
+    this.usersSerivce.loadUsers();
   }
 
 }
