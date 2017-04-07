80a0fb486f5efb13b6e07989815c32ac2a822d64
aurimas-darguzis <aurimas@darguzis.com>
2017-04-07 12:21:13 +0100

    implemented recipe component template

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RecipesService, Recipe } from './../shared';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipesComponent implements OnInit {
+  recipes$: Observable<Recipe[]>;
+  selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(
+    private recipesService: RecipesService
+  ) { }
 
   ngOnInit() {
+    this.recipes$ = this.recipesService.recipes$;
+    this.recipesService.loadRecipes();
+  }
+
+  resetRecipe() {
+    const emptyRecipe: Recipe = { id: null, name: '', ingredients: '', nutrition: '', method: '' };
+    this.selectedRecipe = emptyRecipe;
+  }
+
+  selectRecipe(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
+
+  saveRecipe(recipe: Recipe) {
+    this.recipesService.saveRecipes(recipe);
+    this.resetRecipe();
+  }
+
+  deleteRecipe(recipe: Recipe) {
+    this.recipesService.deleteRecipe(recipe);
+    this.resetRecipe();
   }
 
 }
