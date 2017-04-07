f15fb2808cba9a212a8d4f143c49fa7f4113e08c
aurimas-darguzis <aurimas@darguzis.com>
2017-04-07 12:47:38 +0100

    implemented recpie detail and list components. Done with todays Kata

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../shared';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipeDetailComponent implements OnInit {
+  originalName: string;
+  selectedRecipe: Recipe;
+
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
+
+  @Input() set recipe(value: Recipe) {
+    if (value) { this.originalName = value.name }
+    this.selectedRecipe = Object.assign({}, value);
+  }
 
   constructor() { }
 
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../shared';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipesListComponent implements OnInit {
+  @Input() recipes: Recipe;
+  @Output() selected = new EventEmitter();
+  @Output() deleted = new EventEmitter();
 
   constructor() { }
 
